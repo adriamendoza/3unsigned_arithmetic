08a9ebca29817a18b578de711b6a71af51524d2e
adriamendoza <adriamen1101@gmail.com>
2025-05-02 21:32:59 -0700

    Update unsigned.s

--- unsigned project/unsigned.s
+++ unsigned project/unsigned.s
@@ -17,4 +17,37 @@ dif_result:      .asciz " = %u\n"
 prod_3:          .asciz "%u * %u * %u"
 prod_result:     .asciz " = %u\n"
 
+.align 4
+.section .data
+
+inp1:  .word  0
+inp2:  .word  0
+inp3:  .word  0
+sum:   .word  0
+
+.align 4
+.text
+main:
+
+        push {lr}
+
+        ldr r4, =inp1
+        ldr r5, =inp2
+        ldr r6, =inp3
+
+        ldr r0, =prompt
+        bl printf
+
+        ldr r0, =scan_str
+        ldr r1, =inp1
+        ldr r2, =inp2
+        ldr r3, =inp3
+        bl scanf
+
+        ldr r0, =output_str
+        ldr r1, [r4]
+        ldr r2, [r5]
+        ldr r3, [r6]
+        bl printf
+
 
