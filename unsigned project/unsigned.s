7acb575466875bf866b2386eebd820d5a3e40051
adriamendoza <adriamen1101@gmail.com>
2025-05-02 21:28:51 -0700

    Update unsigned.s

--- unsigned project/unsigned.s
+++ unsigned project/unsigned.s
@@ -9,4 +9,12 @@ scan_str:        .asciz "%u %u %u"
 output_str:      .asciz "You entered %u, %u, and %u.\n"
 
 add_3:           .asciz "%u + %u + %u"
-sum_result:      .asciz
+sum_result:      .asciz " = %u\n"
+
+dif_3:           .asciz "%u - %u - %u"
+dif_result:      .asciz " = %u\n"
+
+prod_3:          .asciz "%u * %u * %u"
+prod_result:     .asciz " = %u\n"
+
+
